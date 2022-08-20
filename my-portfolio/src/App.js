44d528b0af070f9e3b5cb67605566e50364f06fe
dialloflatno <brahimdiallo23@gmail.com>
2022-08-20 18:47:07 -0400

    realigned

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <div>
+      <div className='top-stack-logo-phrase'>
         <div className="firststack">
           <div className="phrases">
             <span>I BE SMILING</span>
